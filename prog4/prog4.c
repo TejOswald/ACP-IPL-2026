3163986942f0864cc16a5ea92f3e6a4e1a1ee3f2
Tejas.Sandesh <tejassandesh08@gmail.com>
2026-05-26 09:05:25 +0530

    Program 4 completed

---
+++ prog4/prog4.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+typedef struct {
+    unsigned firstbit : 1;
+    unsigned secondbit : 3;
+    unsigned thirdbit : 4;
+} Bitfield;
+
+int main(void) {
+    Bitfield a = {0, 0, 0};
+    a.firstbit = 1;
+    a.secondbit = 5;
+    a.thirdbit = 9;
+
+    printf("firstbit = %u\n", a.firstbit);
+    printf("secondbit = %u\n", a.secondbit);
+    printf("thirdbit = %u\n", a.thirdbit);
+    printf("sizeof(Bitfield) = %zu bytes\n", sizeof a);
+    return 0;
+}
